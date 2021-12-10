e7007192168ad920a82f6b1e444fd6cad3ff644d
Prabesh <prabesh.bista369@gmail.com>
2021-12-10 17:56:39 +0545

    barrel file,weather model,location model

---
+++ weather/packages/meta_weather_api/lib/src/models/location.dart
@@ -0,0 +1,21 @@
+enum LocationType { city, region, state, province, country, continent }
+
+class Location {
+  final String title;
+  final LocationType locationtype;
+  final int woeid;
+  final LattLong lattlong;
+
+  const Location(
+      {required this.title,
+      required this.locationtype,
+      required this.woeid,
+      required this.lattlong});
+}
+
+class LattLong {
+  final double latitude;
+  final double longitude;
+
+  const LattLong({required this.latitude, required this.longitude});
+}
