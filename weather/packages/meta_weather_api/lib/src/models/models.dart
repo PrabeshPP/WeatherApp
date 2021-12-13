c1019721a5e925e7781686429204e060ce7723d7
Prabesh <prabesh.bista369@gmail.com>
2021-12-13 20:06:10 +0545

    file re located

--- weather/packages/meta_weather_api/lib/src/models/models.dart
+++ weather/packages/meta_weather_api/lib/src/models/models.dart
@@ -1,4 +1,4 @@
 //this barrel file would help to maintain the cleanliness while importing the file
 
-export '../../../../../lib/location.dart';
-export '../../../../../lib/weather.dart';
+export 'location.dart';
+export 'weather.dart';
