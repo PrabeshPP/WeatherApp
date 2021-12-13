a18f14a7df3838bed8698b9b869f6892de6187aa
Prabesh <prabesh.bista369@gmail.com>
2021-12-13 19:55:13 +0545

    changes the location

--- weather/lib/weather.dart
+++ weather/lib/weather.dart
@@ -1,6 +1,7 @@
 /* this file would return the weather data of the entered place */
 
 import 'package:json_annotation/json_annotation.dart';
+
 part 'weather.g.dart';
 
 enum WeatherState {
@@ -43,24 +44,45 @@ enum WindDirectionCompass {
   @JsonValue('S')
   south,
   @JsonValue('SW')
-  soouthwest,
+  southWest,
   @JsonValue('W')
   west,
   @JsonValue('NW')
   northWest,
-  unknown
+  unknown,
 }
 
 @JsonSerializable()
 class Weather {
-  final String id;
+  const Weather({
+    required this.id,
+    required this.weatherStateName,
+    required this.weatherStateAbbr,
+    required this.windDirectionCompass,
+    required this.created,
+    required this.applicableDate,
+    required this.minTemp,
+    required this.maxTemp,
+    required this.theTemp,
+    required this.windSpeed,
+    required this.windDirection,
+    required this.airPressure,
+    required this.humidity,
+    required this.visibility,
+    required this.predictability,
+  });
+
+  factory Weather.fromJson(Map<String, dynamic> json) =>
+      _$WeatherFromJson(json);
+
+  final int id;
   final String weatherStateName;
   @JsonKey(unknownEnumValue: WeatherState.unknown)
   final WeatherState weatherStateAbbr;
   @JsonKey(unknownEnumValue: WindDirectionCompass.unknown)
   final WindDirectionCompass windDirectionCompass;
-  final String created;
-  final String applicableDate;
+  final DateTime created;
+  final DateTime applicableDate;
   final double minTemp;
   final double maxTemp;
   final double theTemp;
@@ -70,23 +92,4 @@ class Weather {
   final int humidity;
   final double visibility;
   final int predictability;
-
- const  Weather(
-      {required this.id,
-      required this.weatherStateName,
-      required this.weatherStateAbbr,
-      required this.windDirectionCompass,
-      required this.created,
-      required this.applicableDate,
-      required this.minTemp,
-      required this.maxTemp,
-      required this.theTemp,
-      required this.windSpeed,
-      required this.windDirection,
-      required this.airPressure,
-      required this.humidity,
-      required this.visibility,
-      required this.predictability});
-  factory Weather.fromJson(Map<String, dynamic> json) =>
-      _$WeatherFromJson(json);
 }
