5e8db454395dedc5dfaafdf4701a94fafa4db423
Prabesh <prabesh.bista369@gmail.com>
2021-12-16 17:23:23 +0545

    file relocate

--- weather/packages/meta_weather_api/lib/meta_weather_api.dart
+++ weather/packages/meta_weather_api/lib/meta_weather_api.dart
@@ -2,5 +2,5 @@
 
 library meta_weather_api;
 
-export 'src/models/models.dart';
 export 'src/meta_weather_api_client.dart';
+export 'src/models/models.dart';
---
+++ weather/packages/meta_weather_api/test/location_test.dart
@@ -0,0 +1,14 @@
+
+
+// import 'package:test/test.dart';
+
+
+// void main() {
+//   group('Location', () {
+//     group('fromJson', () {
+//       test('throws CheckedFromJsonException when enum is unknown', () {
+//         expect(()=>Location.fromJson)
+//       });
+//     });
+//   });
+// }
