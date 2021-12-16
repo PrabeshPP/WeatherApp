c6c3e925b821cfe233c4f740cfde89fbf41b58ef
Prabesh <prabesh.bista369@gmail.com>
2021-12-16 16:55:41 +0545

    changed file

--- weather/packages/meta_weather_api/lib/src/meta_weather_api_client.dart
+++ weather/packages/meta_weather_api/lib/src/meta_weather_api_client.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 import 'dart:convert';
-import 'package:meta_weather_api/meta_weather_api.dart';
+
 import 'package:http/http.dart' as http;
+
+import '../meta_weather_api.dart';
 
 class LocationIdRequestFailure implements Exception {}
 
