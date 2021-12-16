3397aef7fcd267a0d5496d59a3bdcdb2c7d43793
Prabesh <prabesh.bista369@gmail.com>
2021-12-16 16:50:56 +0545

    changed

--- weather/packages/test/location_test.dart
+++ weather/packages/test/location_test.dart
@@ -1,19 +1,2 @@
-import 'package:json_annotation/json_annotation.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 
-void main() {
-  group('Location', () {
-    group('fromJson', () {
-      test('throws Checked FromJsonException when enum is unknown', () {
-        expect(
-            () => Location.fromJson(<String, dynamic>{
-                  'title': 'mock_title',
-                  'location_type': 'Unknown',
-                  'latt_long': '-34.75,83.28',
-                  'woeid': 42
-                }),
-            throwsA(isA<CheckedFromJsonException>()));
-      });
-    });
-  });
-}
