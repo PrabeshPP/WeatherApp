5b9a6c73d16d258c9babcbd95674ee4994f571b2
Prabesh <prabesh.bista369@gmail.com>
2021-12-16 17:28:29 +0545

    wrote unit test for location model

--- weather/packages/meta_weather_api/lib/src/meta_weather_api_client.dart
+++ weather/packages/meta_weather_api/lib/src/meta_weather_api_client.dart
@@ -2,8 +2,9 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:meta_weather_api/meta_weather_api.dart';
 
-import '../meta_weather_api.dart';
+
 
 class LocationIdRequestFailure implements Exception {}
 
