2c3418ad9f10afc65fa4e9dbf8a6d9bdd3cd7186
Prabesh <prabesh.bista369@gmail.com>
2021-12-17 09:44:32 +0545

    implemented group unit test

--- weather/test/counter_test.dart
+++ weather/test/counter_test.dart
@@ -2,9 +2,21 @@ import 'package:test/test.dart';
 import 'package:weather/counter.dart';
 
 void main() {
-  test("Counter Value should be incremented", () {
-    final counter = Counter();
-    counter.increment();
-    expect(counter.value, 1);
+  group('Counter', () {
+    test('Value Should Start at 0', () {
+      final counter = Counter();
+      expect(counter.value, 0);
+    });
+
+    test("Value should be incremented", () {
+      final counter = Counter();
+      counter.increment();
+      expect(counter.value, 1);
+    });
+    test("Value should be decremented", () {
+      final counter = Counter();
+      counter.decrement();
+      expect(counter.value, -1);
+    });
   });
 }
