7f2e0ef4f01df41f376dac94dbb7a096b8f7123c
Prabesh <prabesh.bista369@gmail.com>
2021-12-19 15:31:27 +0545

    extension,barrel file,weather_repository

--- weather/packages/weather_repository/lib/src/weather_repository.dart
+++ weather/packages/weather_repository/lib/src/weather_repository.dart
@@ -1,11 +1,45 @@
 import 'package:meta_weather_api/meta_weather_api.dart' hide Weather;
+import 'package:weather_repository/weather_repository.dart';
 
 class WeatherFailure implements Exception {}
 
 class WeatherRepository {
   final MetaWeatherApiClientt _metaWeatherApiClientt;
 
-  WeatherRepository(MetaWeatherApiClientt? metaWeatherApiClientt):_metaWeatherApiClientt=metaWeatherApiClientt ?? MetaWeatherApiClientt();
+  WeatherRepository({MetaWeatherApiClientt? metaWeatherApiClientt})
+      : _metaWeatherApiClientt =
+            metaWeatherApiClientt ?? MetaWeatherApiClientt();
 
-  Future<Weather> getWeather
+  Future<Weather> getWeather(String city) async {
+    final location = await _metaWeatherApiClientt.locationSearch(city);
+    final woeid = location.woeid;
+    final weather = await _metaWeatherApiClientt.weatherSearch(woeid);
+    return Weather(
+        location: weather.weatherStateName,
+        weatherCondition: weather.weatherStateAbbr.toCondition,
+        temperature: weather.theTemp);
+  }
+}
+
+extension on WeatherState {
+  WeatherCondition get toCondition {
+    switch (this) {
+      case WeatherState.clear:
+        return WeatherCondition.clear;
+      case WeatherState.snow:
+      case WeatherState.sleet:
+      case WeatherState.hail:
+        return WeatherCondition.snowy;
+      case WeatherState.thunderstorm:
+      case WeatherState.heavyRain:
+      case WeatherState.lightRain:
+      case WeatherState.showers:
+        return WeatherCondition.rainy;
+      case WeatherState.heavyCloud:
+      case WeatherState.lightCloud:
+        return WeatherCondition.cloudy;
+      default:
+        return WeatherCondition.unknown;
+    }
+  }
 }
--- weather/packages/weather_repository/lib/weather_repository.dart
+++ weather/packages/weather_repository/lib/weather_repository.dart
@@ -1,2 +1,5 @@
+//Barrel file for weather repository
+
 library weather_repository;
 export 'src/models/weather.dart';
+export 'src/weather_repository.dart';
