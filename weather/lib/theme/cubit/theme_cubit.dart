bd48b0960c57edc7fcbc6f1c63efdefcd699c7f2
Prabesh <prabesh.bista369@gmail.com>
2021-12-20 13:18:08 +0545

    cubit theme

--- weather/lib/theme/cubit/theme_cubit.dart
+++ weather/lib/theme/cubit/theme_cubit.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:weather/weather/models/weather.dart';
-import 'package:weather_repository/weather_repository.dart' as Weather1;
+import 'package:weather_repository/weather_repository.dart' as weather1;
 
 class ThemeCubit extends HydratedCubit<Color> {
   ThemeCubit() : super(defaultColor);
@@ -25,15 +25,15 @@ class ThemeCubit extends HydratedCubit<Color> {
 extension on Weather {
   Color get toColor {
     switch (condition) {
-      case Weather1.WeatherCondition.clear:
+      case weather1.WeatherCondition.clear:
         return Colors.orangeAccent;
-      case Weather1.WeatherCondition.snowy:
+      case weather1.WeatherCondition.snowy:
         return Colors.lightBlueAccent;
-      case Weather1.WeatherCondition.cloudy:
+      case weather1.WeatherCondition.cloudy:
         return Colors.blueGrey;
-      case Weather1.WeatherCondition.rainy:
+      case weather1.WeatherCondition.rainy:
         return Colors.indigoAccent;
-      case Weather1.WeatherCondition.unknown:
+      case weather1.WeatherCondition.unknown:
       default:
         return ThemeCubit.defaultColor;
     }
--- weather/lib/theme/theme.dart
+++ weather/lib/theme/theme.dart
@@ -1,1 +1,2 @@
 export 'cubit/theme_cubit.dart';
+//barrel file for cubit_theme.dart file
