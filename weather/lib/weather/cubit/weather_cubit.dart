7eb035cd821e285ccfe5dd8ccbab4c6c062dd012
Prabesh <prabesh.bista369@gmail.com>
2021-12-20 10:23:15 +0545

    weather_cubit

---
+++ weather/lib/weather/cubit/weather_cubit.dart
@@ -0,0 +1,27 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:weather/weather/models/models.dart';
+import 'package:weather_repository/weather_repository.dart'
+    show WeatherRepository;
+
+part 'weather_cubit.g.dart';
+part 'weather_state.dart';
+
+class WeatherCubit extends HydratedCubit<WeatherState> {
+  final WeatherRepository _weatherRepository;
+  WeatherCubit(this._weatherRepository) : super(WeatherState());
+
+  @override
+  WeatherState? fromJson(Map<String, dynamic> json) {
+    // TODO: implement fromJson
+    throw UnimplementedError();
+  }
+
+  @override
+  Map<String, dynamic>? toJson(WeatherState state) {
+    // TODO: implement toJson
+    throw UnimplementedError();
+  }
+}
---
+++ weather/lib/weather/cubit/weather_state.dart
@@ -0,0 +1,37 @@
+part of 'weather_cubit.dart';
+
+enum WeatherStatus { initial, loading, success, failure }
+
+extension WeatherStateX on WeatherStatus {
+  bool get isInitial => this == WeatherStatus.initial;
+  bool get isLoading => this == WeatherStatus.loading;
+  bool get isSuccess => this == WeatherStatus.success;
+  bool get isFailure => this == WeatherStatus.failure;
+}
+
+@JsonSerializable()
+class WeatherState extends Equatable {
+  final WeatherStatus status;
+  final TemperatureUnits temperatureUnits;
+  final Weather weather;
+
+  factory WeatherState.fromJson(Map<String, dynamic> json) =>
+      _$WeatherStateFromJson(json);
+
+  WeatherState(
+      {this.status = WeatherStatus.initial,
+      this.temperatureUnits = TemperatureUnits.celsius,
+      Weather? weather})
+      : weather = weather ?? Weather.empty;
+
+  WeatherState copyWith(
+      {WeatherStatus? status, TemperatureUnits? temperatureUnits, Weather? weather}) {
+    return WeatherState(
+        weather: weather ?? this.weather,
+        temperatureUnits: temperatureUnits ?? this.temperatureUnits,
+       status: status ?? this.status);
+  }
+
+  @override
+  List<Object?> get props => [status, temperatureUnits, weather];
+}
